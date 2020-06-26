b193f8d1cb1cd39cefb39af64508d045ec0651b7
Роберт <zajnitdinov@icloud.com>
2020-06-26 16:51:04 +0300

    *

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './scss/index.scss';
+
+const dropdownMenu = document.getElementsByClassName('dropdown__menu__tag')[0];
+const searchInputTag = document.getElementsByClassName('search__input__tags__field')[0];
+
+dropdownMenu.style.display = 'none';
+
+searchInputTag.onclick = function () {
+    dropdownMenu.style.display = 'inline-block'
+    dropdownMenu.style.transition = '1000ms ease-in'
+}
